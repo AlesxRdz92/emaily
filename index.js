0d24983264c491a8e33a8843295bc3275ca058ce
Alejandro Gongora (Contractor) <x244911@LUV.AD.SWACORP.COM>
2018-09-25 14:53:16 -0500

    Auth completed

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const coockieSession = require('cookie-session');
+const passport = require('passport');
 const PORT = process.env.PORT || 5000;
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
+app.use(
+    coockieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.coockieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,23 @@
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => done(null, user));
+});
 
 passport.use(new Strategy({
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-	console.log(accessToken);
-	console.log('refresh', refreshToken);
-	console.log('profile', profile);
+	User.findOne({ googleId: profile.id }).then(user => {
+		if (user)
+			done(null, user);
+		else
+			new User({ googleId: profile.id }).save().then(userNew => done(null, userNew));
+	}).catch(e => console.log(e));
 }));
